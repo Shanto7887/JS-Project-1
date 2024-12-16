dd514bb32e8651cdb9da0766daefc89f98852666
Rakibul Hassan Shanto <mdshanto7887@gmail.com>
2024-12-16 12:50:55 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+
+
+let a
+let date
+let time
+const options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  }
+
+setInterval(() => {
+
+
+    a = new Date();
+    date = a.toLocaleDateString(undefined,options)
+    time = a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds()
+    document.getElementById('1').innerHTML = time + '<br> on ' + date
+
+}, 1000)
+
